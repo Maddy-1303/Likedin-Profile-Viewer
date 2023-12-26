95a5cf82cd6887adf1fcec685a03c143cc0cbb23
Madhan <madhanprasath@mallow-tech.com>
2023-12-26 20:50:33 +0530

    Extracted the datas in the local storage

--- content_script.js
+++ content_script.js
@@ -1,61 +1,31 @@
-// Function to capture LinkedIn profiles
-function captureLinkedInProfiles() {
+document.addEventListener('DOMContentLoaded', function () {
+
     const profiles = Array.from(document.querySelectorAll('.flex.flex-column'));
-    const linkedinProfiles = profiles.map(profile => {
-        const name = profile.querySelector('.artdeco-entity-lockup__title a span')?.textContent.trim();
-        const photoUrl = profile.querySelector('.artdeco-entity-lockup__image img')?.src;
-        const organizationName = profile.querySelector('.artdeco-entity-lockup__badge + .artdeco-entity-lockup__subtitle')?.textContent.trim();
+    const linkedinPeople = profiles.map(profile => {
+        const profileLink = profile.querySelector('.artdeco-entity-lockup__title a')?.getAttribute('href');
+        const name = profile.querySelector('.artdeco-entity-lockup__title span')?.textContent.trim();
+        const photoUrl = profile.querySelector('.artdeco-entity-lockup__image img')?.getAttribute('src');
+        const orgLinkElement = profile.querySelector('.artdeco-entity-lockup__subtitle a');
+        const organizationName = orgLinkElement?.textContent.trim();
+        const organizationLinkedInUid = orgLinkElement?.getAttribute('href')?.match(/\/company\/(\d+)/)?.[1];
         const title = profile.querySelector('.artdeco-entity-lockup__highlight-keyword')?.textContent.trim();
-        const location = profile.querySelector('.artdeco-entity-lockup__caption')?.textContent.trim();
+        const locationElement = profile.querySelector('.artdeco-entity-lockup__caption');
+        const presentRawAddress = locationElement?.textContent.trim();
 
         return {
+            href: `https://www.linkedin.com${profileLink}`,
             name,
-            photoUrl,
-            organizationName,
+            photo_url: photoUrl,
+            organization_name: organizationName,
+            organization_linkedin_uid: organizationLinkedInUid,
             title,
-            location,
+            present_raw_address: presentRawAddress,
         };
     });
 
-    // Retrieve existing profiles from localStorage
-    const existingProfiles = JSON.parse(localStorage.getItem('linkedinProfiles')) || [];
-
-    // Combine existing and new profiles, and store in localStorage
-    const allProfiles = existingProfiles.concat(linkedinProfiles);
-    localStorage.setItem('linkedinProfiles', JSON.stringify(allProfiles));
-
     // Display the extracted data in the console
-    console.log("LinkedIn profiles captured:", allProfiles);
-}
+    console.log("LinkedIn people captured:", linkedinPeople);
 
-// Function to scroll to the bottom of the page
-function scrollToBottom() {
-    window.scrollBy(0, window.innerHeight);
-}
-
-// Function to check for new profiles and capture them
-function checkAndCaptureProfiles() {
-    const newProfiles = document.querySelectorAll('.flex.flex-column');
-    const existingProfileCount = JSON.parse(localStorage.getItem('linkedinProfiles'))?.length || 0;
-
-    if (newProfiles.length > existingProfileCount) {
-        // Capture the new profiles
-        captureLinkedInProfiles();
-
-        // Scroll to load more profiles
-        scrollToBottom();
-    } else {
-        // Stop the interval if no new profiles are found
-        clearInterval(intervalId);
-    }
-}
-
-// Initial capture when the page is loaded
-captureLinkedInProfiles();
-
-// Scroll to load more profiles
-scrollToBottom();
-
-// Check for new profiles every second
-const scrollIncrement = 500; // Adjust the scroll increment as needed
-const intervalId = setInterval(checkAndCaptureProfiles, 1000);
+    // Store in localStorage
+    localStorage.setItem('linkedinPeople', JSON.stringify(linkedinPeople));
+});
